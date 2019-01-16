574307c257be4c51af6c39a8331e9f8b4f414f4c
ga65seb <xingxing.zhou@tum.de>
2019-01-16 12:28:53 +0100

    Notary

---
+++ src/main/java/com/example/guestbook/Notary.java
@@ -0,0 +1,16 @@
+package com.example.guestbook;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Notary {
+	public ArrayList<Double> attendance_database;
+	
+	
+	public Notary() {
+		Random r = new Random();
+		double h_0 = r.nextDouble();
+		attendance_database.add(h_0);
+		
+	}
+}
